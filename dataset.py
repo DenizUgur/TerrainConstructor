bf4b1e9d602fbef7754e0b4bd7c7f5eda83f28ff
Deniz Ugur <deniz343@gmail.com>
2020-12-25 13:38:09 +0300

    initial dataset commit

--- dataset.py
+++ dataset.py
@@ -1,36 +1,135 @@
 import rasterio
 from scipy.ndimage import zoom
+from skimage.draw import rectangle_perimeter, line
 from tqdm import tqdm
 import numpy as np
+from glob import glob
+import random
 
 
 class Dataset:
-    def __init__(self) -> None:
-        self.sample = "data/MDRS/data/USGS_one_meter_x51y425_UT_Southern_QL1_2018.tif"
-
-    def blockshaped(self, arr, nrows, ncols):
+    def __init__(self, patch_size=30, sample_size=256, pad=50, block_variance=4, randomize=True):
         """
-        Return an array of shape (n, nrows, ncols) where
-        n * nrows * ncols = arr.size
+        patch_size -> the 1m^2 area to read from .TIF
+        sample_size -> the 0.1m^2 res area to be trained sample size
+        block_variance -> how many different observer points
+        pad -> n pixels to pad before getting a random observer
+        """
+        np.seterr(divide="ignore", invalid="ignore")
+
+        # * Set Dataset attributes
+        self.observer_height = 0.75
+        self.patch_size = patch_size
+        self.sample_size = sample_size
+        self.block_variance = block_variance
+        self.pad = pad
+
+        # * Gather files
+        self.files = glob("data/MDRS/data/*.tif")
+        self.randomize = randomize
+        if self.randomize:
+            random.shuffle(self.files)
+
+        # * Keep track of requested files
+        self.last_req_index = -1
+        self.blocks = None
+
+    def prepare(self):
+        if self.blocks is None or self.last_req_index == len(self.blocks):
+            # * Load new file
+            if len(self.files) == 0:
+                return False
+
+            self.blocks = self.get_blocks(self.files.pop())
+            if self.randomize:
+                random.shuffle(self.blocks)
+
+        return True
+
+    def get(self, index=None, batch=None, return_observer=False):
+        """
+        returns Viewsheded and Ground Truth
+        and optionally observer point
+        """
+        if index is None:
+            self.last_req_index += 1
+            index = self.last_req_index
+
+        adjusted = self.get_adjusted(self.blocks[index])
+        viewsheded, observer = self.viewshed(adjusted, index)
+
+        if return_observer:
+            return viewsheded, adjusted, observer
+        return viewsheded, adjusted
+
+    def viewshed(self, dem, seed):
+        h, w = dem.shape
+        np.random.seed(seed)
+        rands = np.random.rand(h - self.pad, w - self.pad)
+        template = np.zeros_like(dem)
+        template[
+            self.pad - self.pad // 2 : h - self.pad // 2,
+            self.pad - self.pad // 2 : w - self.pad // 2,
+        ] = rands
+        observer = tuple(np.argwhere(template == np.max(template))[0])
+
+        yp, xp = observer
+        zp = dem[observer] + self.observer_height
+        observer = (xp, yp, zp)
+        viewshed = np.copy(dem)
+
+        # * Find perimiter
+        rr, cc = rectangle_perimeter((1, 1), end=(h - 2, w - 2), shape=dem.shape)
+
+        # * Iterate through perimiter
+        for yc, xc in zip(rr, cc):
+            # * Form the line
+            ray_y, ray_x = line(yp, xp, yc, xc)
+            ray_z = dem[ray_y, ray_x]
+
+            m = (ray_z - zp) / np.hypot(ray_y - yp, ray_x - xp)
+
+            max_so_far = -np.inf
+            for yi, xi, mi in zip(ray_y, ray_x, m):
+                if mi < max_so_far:
+                    viewshed[yi, xi] = np.nan
+                else:
+                    max_so_far = mi
+
+        return viewshed, observer
+
+    def blockshaped(self, arr, nside):
+        """
+        Return an array of shape (n, nside, nside) where
+        n * nside * nside = arr.size
 
         If arr is a 2D array, the returned array should look like n subblocks with
         each subblock preserving the "physical" layout of arr.
         """
         h, w = arr.shape
-        assert h % nrows == 0, "{} rows is not evenly divisble by {}".format(h, nrows)
-        assert w % ncols == 0, "{} cols is not evenly divisble by {}".format(w, ncols)
+        assert h % nside == 0, "{} rows is not evenly divisble by {}".format(h, nside)
+        assert w % nside == 0, "{} cols is not evenly divisble by {}".format(w, nside)
         return (
-            arr.reshape(h // nrows, nrows, -1, ncols)
+            arr.reshape(h // nside, nside, -1, nside)
             .swapaxes(1, 2)
-            .reshape(-1, nrows, ncols)
+            .reshape(-1, nside, nside)
         )
 
-    def get_interpolated(self, blocks, factor=10):
-        for block in tqdm(blocks, total=len(blocks), ncols=100):
-            yield zoom(block, factor, order=1)
+    def get_adjusted(self, block):
+        _, block = block
+        zoomed = zoom(block, 10, order=1)
+        y, x = zoomed.shape
+        startx = x // 2 - (self.sample_size // 2)
+        starty = y // 2 - (self.sample_size // 2)
+        return zoomed[
+            starty : starty + self.sample_size, startx : startx + self.sample_size
+        ]
 
-    def get_blocks(self, size):
-        raster = rasterio.open(self.sample)
+    def get_blocks(self, file):
+        if self.patch_size % 2 == 0:
+            self.patch_size += 1
+
+        raster = rasterio.open(file)
         grid = raster.read(1)
 
         # Remove minimum
@@ -44,22 +143,25 @@ class Dataset:
 
         w, h = grid.shape
         if NL > NR:
-            grid = grid[w % size : w, 0:h]
+            grid = grid[w % self.patch_size : w, 0:h]
         else:
-            grid = grid[0 : w - (w % size), 0:h]
+            grid = grid[0 : w - (w % self.patch_size), 0:h]
 
         w, h = grid.shape
         if NT > NB:
-            grid = grid[0:w, h % size : h]
+            grid = grid[0:w, h % self.patch_size : h]
         else:
-            grid = grid[0:w, 0 : h - (h % size)]
+            grid = grid[0:w, 0 : h - (h % self.patch_size)]
 
-        blocks = self.blockshaped(grid, size, size)
+        blocks = self.blockshaped(grid, self.patch_size)
         usable_blocks = list(
             filter(lambda x: np.count_nonzero(np.isnan(x)) == 0, blocks)
         )
-        usable_px = len(usable_blocks) * size * size
-        all_px = np.count_nonzero(~np.isnan(grid))
-        ratio = 1 - (usable_px / all_px)
 
-        return usable_blocks, ratio
+        # * Increase variation
+        final_blocks = []
+        for block in usable_blocks:
+            for _ in range(self.block_variance):
+                final_blocks.append((len(final_blocks), block))
+
+        return final_blocks
--- test.py
+++ test.py
@@ -1,22 +1,47 @@
+import numpy as np
 from dataset import Dataset
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from timeit import default_timer as dt
+
+
+def main(index):
+    global Y
+    start = dt()
+    DS.prepare()
+    print("Prepared in {:.2f} seconds".format(dt() - start))
+
+    start = dt()
+    X, Y, O = DS.get(index=index, return_observer=True)
+    print("Got a sample in {:.2f} seconds".format(dt() - start))
+
+    # * PLOT
+    fig = plt.figure(figsize=(32, 18))
+    ax = plt.axes(projection="3d")
+    lin_x = np.arange(0, X.shape[0], 1)
+    lin_y = np.arange(0, X.shape[1], 1)
+    x, y = np.meshgrid(lin_x, lin_y)
+
+    colors = np.empty(X.shape, dtype=str)
+    colors[np.isnan(X)] = 'r'
+    colors[~np.isnan(X)] = 'y'
+
+    ax.plot_surface(x, y, X, rstride=5, cstride=5)
+    ax.scatter(O[0], O[1], O[2], s=50, color="red")
+
+    fig.tight_layout()
+    plt.show(block=False)
+
 
 if __name__ == "__main__":
     DS = Dataset()
 
-    blocks, loss = DS.get_blocks(30)
-    print("Data loss after blockify {:.2f}%".format(100 * loss))
-
-    #! Temporarily reduce size of blocks
-    blocks = blocks[:30]
-
-    interp_blocks = list(DS.get_interpolated(blocks))
-
-    dtm = interp_blocks[0]
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-    ax1.contourf(dtm, levels=100, cmap="terrain")
-    ax2.contourf(blocks[0], levels=100, cmap="jet")
-
-    fig.tight_layout()
-    plt.show()
+    i = 0
+    while 1:
+        print(i)
+        main(i)
+        no = str(input("Continue? "))
+        plt.close()
+        if no == "n":
+            exit(0)
+        i += 1
